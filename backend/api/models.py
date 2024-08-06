a41a5c478260793d3c7f8beba4c476da6fd0fab5
Samet Koleoglu <sametkoleoglu@192.168.1.38>
2024-08-06 16:17:21 +0300

    Blog Site :)

---
+++ backend/api/models.py
@@ -0,0 +1,181 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from django.db.models.signals import post_save
+from django.utils.text import slugify
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self) -> str:
+        return self.username
+
+    def save(self, *args, **kwargs):
+        email_username, mobile = self.email.split("@")
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = email_username
+        if self.username == "" or self.username == None:
+            self.username = email_username
+
+        super(User, self).save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(
+        default="default/default.jpg", upload_to="profile_pics", null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    about = models.CharField(max_length=100, null=True, blank=True)
+    author = models.BooleanField(default=False)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.CharField(max_length=100, null=True, blank=True)
+    twitter = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = self.user.full_name
+
+        super(Profile, self).save(*args, **kwargs)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(null=True, upload_to='image', blank=True)
+    slug = models.SlugField(null=True, blank=True,unique=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    # class Meta:
+    #     ordering = ['-date']
+    #     verbose_name_plural = 'Category'
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+        
+    
+    
+    def post_count(self):
+        return Post.objects.filter(category=self).count()
+    
+class Post(models.Model):
+    
+    STATUS = (
+        ('Active', 'Active'),
+        ('Draft', 'Draft'),
+        ('Disable', 'Disableb'),
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,blank=True, null=True)
+    title = models.CharField(max_length=100)
+    tags = models.CharField(max_length=100,null=True, blank=True)
+    image = models.FileField(null=True, upload_to='image', blank=True)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=100, choices=STATUS, default='Active')
+    view = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, related_name='likes_user', blank=True)
+    slug = models.SlugField(null=True, blank=True,unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    
+    class Meta:
+        ordering = ['-date']
+        verbose_name_plural = 'Post'
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) + "-" + shortuuid.uuid()[:2]  #Atılan postu benzersiz kılmak içn bu işlemi yaptık !
+        super(Post, self).save(*args, **kwargs)
+        
+    def comments(self):
+        return Comment.objects.filter(post=self).order_by("-id")
+        
+        
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    comment = models.TextField(null=True, blank=True)
+    reply = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.post.title
+    
+    class Meta:
+        verbose_name_plural = 'Comment'
+        ordering = ['-date']
+      
+        
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.post.title
+    
+    class Meta:
+        verbose_name_plural = 'Bookmark'
+        ordering = ['-date']
+        
+        
+
+class Notification(models.Model):
+    NOTI_TYPE = (
+        ('Like', 'Like'),
+        ('Comment', 'Comment'),
+        ('Bookmark', 'Bookmark'),
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    type = models.CharField(choices=NOTI_TYPE,max_length=100)
+    seen = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        if self.post:
+            return f"{self.type} - {self.post.title}"
+        else:
+            return "Notification"
+        
+    
+    class Meta:
+        verbose_name_plural = 'Notification'
+        ordering = ['-date']
---
+++ frontend/src/views/core/Detail.jsx
@@ -0,0 +1,265 @@
+import React, { useEffect, useState } from "react";
+import Header from "../partials/Header";
+import Footer from "../partials/Footer";
+import { Link, useParams } from "react-router-dom";
+import apiInstance from "../../utils/axios";
+import Moment from "../../plugin/Moment";
+import Toast from "../../plugin/Toast";
+
+
+
+function Detail() {
+
+  const param = useParams();
+
+  // STATES
+  const [post, setPost] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [createComment, setCreateComment] = useState({
+    full_name: "",
+    email: "",
+    comment: "",
+  });
+
+
+  const fetchPost = async () => {
+    const response = await apiInstance.get(`/post/detail/${param.slug}/`);
+    setPost(response.data);
+
+    const tagArray = response?.data?.tags?.split(",");
+    setTags(tagArray);
+  };
+
+  const handleCreateComment = async (e) => {
+    setCreateComment({
+      ...createComment,
+      [e.target.name]: e.target.value,
+      
+    })
+    console.log(createComment);
+  }
+
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault()
+
+    const json = {
+      post_id : post?.id,
+      name: createComment.full_name,
+      email: createComment.email,
+      comment: createComment.comment
+    }
+
+    const response = await apiInstance.post('post/comment-post/', json)
+    console.log(response);
+    Toast("success", "Commented Successfully");
+    fetchPost();
+
+    setCreateComment({
+      full_name: "",
+      email: "",
+      comment: "",
+    })
+      
+  }
+
+  const handleLikePost = async () => {
+    const json = {
+      user_id : 4,
+      post_id : post?.id
+    }
+
+    const response = await apiInstance.post('post/like-post/', json)
+    console.log(response.data);
+    Toast("success", response.data.message);
+    fetchPost();
+  }
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <section className=" mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <a href="#" className="badge bg-danger mb-2 text-decoration-none">
+                <i className="small fw-bold " />
+                Lifestyle
+              </a>
+              <h1 className="text-center">{post.title}</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-0">
+        <div className="container position-relative" data-sticky-container="">
+          <div className="row">
+            <div className="col-lg-2">
+              <div
+                className="text-start text-lg-center mb-5"
+                data-sticky=""
+                data-margin-top={80}
+                data-sticky-for={991}
+              >
+                <div className="position-relative">
+                  <div className="avatar avatar-xl">
+                    <img
+                      className="avatar-img"
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                      src={post?.profile?.image}
+                      alt="avatar"
+                    />
+                  </div>
+                  <a
+                    href="#"
+                    className="h5 fw-bold text-dark text-decoration-none mt-2 mb-0 d-block"
+                  >
+                    {post?.profile?.full_name}
+                  </a>
+                  <p>{post?.profile?.bio || " "}</p>
+                </div>
+
+                <hr className="d-none d-lg-block " />
+
+                <ul className="list-inline list-unstyled">
+                  <li className="list-inline-item d-lg-block my-lg-2 text-start">
+                    <i className="fas fa-calendar"></i> {Moment(post?.date)}
+                  </li>
+                  {/* <li className="list-inline-item d-lg-block my-lg-2 text-start">
+                    <i className="fas fa-clock"></i> 5 min read
+                  </li> */}
+                  <li className="list-inline-item d-lg-block my-lg-2 text-start">
+                    <a href="#" className="text-body">
+                      <i className="fas fa-heart me-1" />
+                    </a>
+                    {post?.likes?.length || 0} Likes
+                  </li>
+                  <li className="list-inline-item d-lg-block my-lg-2 text-start">
+                    <i className="fas fa-eye" />
+                    {post?.view} Views
+                  </li>
+                </ul>
+                {/* Tags */}
+                <ul className="list-inline text-primary-hover mt-0 mt-lg-3 text-start">
+                  {tags?.map((tag, index) => (
+                    <li className="list-inline-item" key={index}>
+                      <a
+                        className="text-body text-decoration-none fw-bold"
+                        href="#"
+                      >
+                        #{tag}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+
+                <button onClick={handleLikePost} className="btn btn-success">
+                  <i className="fas fa-thumbs-up me-2"></i>
+                  {post?.likes?.length}
+                </button>
+
+                <button className="btn btn-primary ms-2">
+                  <i className="fas fa-bookmark"></i>
+                </button>
+
+              </div>
+            </div>
+            {/* Left sidebar END */}
+            {/* Main Content START */}
+            <div className="col-lg-10 mb-5">
+              <p>
+                {
+                    post?.description
+                }
+              </p>
+
+                <hr />
+
+              
+
+              <div className="mt-5">
+                <h3>{post?.comments?.length == 1 ? "1 comment" : `${post?.comments?.length} comments`}</h3>
+              
+              {
+                post?.comments?.map((comment, index) => (  
+                <div className="my-4 d-flex bg-light p-3 mb-3 rounded">
+                  {/* <img
+                    className="avatar avatar-md rounded-circle float-start me-3"
+                    src="https://img.freepik.com/free-photo/front-portrait-woman-with-beauty-face_186202-6146.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710979200&semt=ais"
+                    style={{
+                      width: "70px",
+                      height: "70px",
+                      objectFit: "cover",
+                      borderRadius: "50%",
+                    }}
+                    alt="avatar"
+                  /> */}
+                  <div>
+                    <div className="mb-2">
+                      <h5 className="m-0">{comment?.name}</h5>
+                      <span className="me-3 small">{Moment(comment?.date)}.</span>
+                    </div>
+                    <p className="fw-bold">
+                      {comment?.comment}.{" "}
+                    </p>
+                  </div>
+                </div>
+                ))
+              }
+
+              
+              </div>
+              {/* Comments END */}
+              {/* Reply START */}
+              <div className="bg-light p-3 rounded">
+                <h3 className="fw-bold">Leave a reply</h3>
+                <small>
+                  Your email address will not be published. Required fields are
+                  marked *
+                </small>
+                <form className="row g-3 mt-2" onSubmit={handleCommentSubmit}>
+                  <div className="col-md-6">
+                    <label className="form-label">Name *</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      aria-label="Full Name"
+                      onChange={handleCreateComment}
+                      value={createComment.full_name}
+                      name="full_name"
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <label className="form-label">Email *</label>
+                    <input type="email" className="form-control" value={createComment.email} aria-label="Email" name="email" onChange={handleCreateComment} />
+                  </div>
+                  <div className="col-12">
+                    <label className="form-label">Write Comment *</label>
+                    <textarea className="form-control" rows={4} aria-label="Write Comment" value={createComment.comment} name="comment" onChange={handleCreateComment} />
+                  </div>
+                  <div className="col-12">
+                    <button type="submit" className="btn btn-primary">
+                      Post comment <i className="fas fa-paper-plane"></i>
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
+
+export default Detail;
